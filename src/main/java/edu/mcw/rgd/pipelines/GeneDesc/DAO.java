6961f6b395f275263dee3e51d4962b64bfc56c79
Marek Tutaj <mtutaj@mcw.edu>
2019-01-11 10:26:27 -0600

    downloads all automated gene descriptions from AGR, merges them with automated RGD descriptions    and stores both in GENES table in columns AGR_DESC, MERGED_DESC

--- src/main/java/edu/mcw/rgd/pipelines/GeneDesc/DAO.java
+++ src/main/java/edu/mcw/rgd/pipelines/GeneDesc/DAO.java
@@ -2,9 +2,9 @@ package edu.mcw.rgd.pipelines.GeneDesc;
 
 import edu.mcw.rgd.dao.impl.GeneDAO;
 import edu.mcw.rgd.datamodel.Gene;
+import edu.mcw.rgd.process.Utils;
 import org.apache.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,6 +15,7 @@ import java.util.List;
 public class DAO {
 
     GeneDAO gdao = new GeneDAO();
+    Logger logChangedDesc = Logger.getLogger("changedDesc");
 
     public String getConnectionInfo() {
         return gdao.getConnectionInfo();
@@ -22,5 +23,18 @@ public class DAO {
 
     public List<Gene> getGenesForSpecies(int speciesTypeKey) throws Exception {
         return gdao.getActiveGenes(speciesTypeKey);
+    }
+
+    public void updateAgrDesc(Gene gene, String agrDesc, String mergedDesc) throws Exception {
+        logChangedDesc.info(
+            "RGD:"+gene.getRgdId()+", "+gene.getSymbol()+"\n"+
+            "  OLD AGR DESC ["+ Utils.NVL(gene.getAgrDescription(),"")+"]\n"+
+            "  NEW AGR DESC ["+ Utils.NVL(agrDesc,"")+"]\n"+
+            "  OLD MERGED DESC ["+ Utils.NVL(gene.getMergedDescription(),"")+"]\n"+
+            "  NEW MERGED DESC ["+ Utils.NVL(mergedDesc,"")+"]");
+
+        gene.setAgrDescription(agrDesc);
+        gene.setMergedDescription(mergedDesc);
+        gdao.updateGene(gene);
     }
 }
--- src/main/java/edu/mcw/rgd/pipelines/GeneDesc/Main.java
+++ src/main/java/edu/mcw/rgd/pipelines/GeneDesc/Main.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.FileSystemResource;
 
+import java.io.File;
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -63,6 +64,16 @@ public class Main {
     void run(String species) throws Exception {
         log.info("running for species: "+species);
 
+        int genesNotInAgr = 0;
+
+        int genesWithAgrDesc = 0;
+        int newAgrDesc = 0;
+        int upToDateAgrDesc = 0;
+
+        int genesWithMergedDesc = 0;
+        int newMergedDesc = 0;
+        int upToDateMergedDesc = 0;
+
         // validate species
         int speciesTypeKey = SpeciesType.parse(species);
         if( SpeciesType.getTaxonomicId(speciesTypeKey)<=0 ) {
@@ -70,19 +81,131 @@ public class Main {
         }
 
         List<Gene> activeGenes = dao.getGenesForSpecies(speciesTypeKey);
+        log.info("  genes to be processed: "+activeGenes.size());
         Collections.shuffle(activeGenes);
         for( Gene gene: activeGenes ) {
+            Thread.sleep(500); // be nice to AGR: sleep 500ms before making a next web request
 
             String agrCuri = "RGD:"+gene.getRgdId();
             String localFile = downloadGeneFile(agrCuri);
 
-            String auto = parseField(localFile, "automatedGeneSynopsis");
-            if( auto==null ) {
-                System.out.println("*** NO AUTO FOR: "+agrCuri+" ***");
-            } else {
-                System.out.println(agrCuri + " : " + auto);
+            String fileContents = Utils.readFileAsString(localFile);
+            if( Utils.isStringEmpty(fileContents) ) {
+                genesNotInAgr++;
+                new File(localFile).delete();
+                continue;
             }
+
+            String auto = parseField(localFile, "automatedGeneSynopsis");
+
+            if( auto!=null ) {
+                genesWithAgrDesc++;
+            }
+
+            boolean doGeneUpdate = false;
+
+            if( Utils.stringsAreEqualIgnoreCase(auto, gene.getAgrDescription()) ) {
+                upToDateAgrDesc++;
+            } else {
+                newAgrDesc++;
+                doGeneUpdate = true;
+            }
+
+            String mergedDesc = generateMergedDesc(gene, auto);
+            if( !Utils.isStringEmpty(mergedDesc) ) {
+                genesWithMergedDesc++;
+            }
+            if( Utils.stringsAreEqualIgnoreCase(mergedDesc, gene.getMergedDescription()) ) {
+                upToDateMergedDesc++;
+            } else {
+                newMergedDesc++;
+                doGeneUpdate = true;
+            }
+
+            if( doGeneUpdate ) {
+                dao.updateAgrDesc(gene, auto, mergedDesc);
+            }
+
+            new File(localFile).delete();
         }
+
+        log.info("  genes not in AGR: "+genesNotInAgr);
+
+        log.info("  genes with automated AGR desription: "+genesWithAgrDesc);
+        log.info("  genes with new automated AGR desription: "+newAgrDesc);
+        log.info("  genes with up-to-date automated AGR desription: "+upToDateAgrDesc);
+
+        log.info("  genes with automated merged desription: "+genesWithMergedDesc);
+        log.info("  genes with new automated merged desription: "+newMergedDesc);
+        log.info("  genes with up-to-date automated merged desription: "+upToDateMergedDesc);
+    }
+
+    String generateMergedDesc(Gene gene, String agrDesc) throws Exception {
+        String rgdDesc = Utils.getGeneDescription(gene);
+
+        // CASE 1: both AGR and RGD desc are empty
+        if( Utils.isStringEmpty(agrDesc) && Utils.isStringEmpty(rgdDesc) ) {
+            return "";
+        }
+
+        // CASE 2: AGR desc is empty,  RGD desc is not empty
+        if( Utils.isStringEmpty(agrDesc) && !Utils.isStringEmpty(rgdDesc) ) {
+            return rgdDesc;
+        }
+
+        // CASE 3: AGR desc is not empty,  RGD desc is empty
+        if( !Utils.isStringEmpty(agrDesc) && Utils.isStringEmpty(rgdDesc) ) {
+            return agrDesc;
+        }
+
+        // CASE 4: merge RGD desc with AGR desc
+        // pathway could be followed by phenotype/disease, cc, chebi
+        String pathwayDesc = null;
+        String chebiDesc = null;
+
+        String pathwayPattern = "PARTICIPATES IN ";
+        String phenoDiseasePattern = "ASSOCIATED WITH ";
+        String ccPattern = "FOUND IN ";
+        String chebiPattern = "INTERACTS WITH "; // always last pattern
+        int pathwayPos = rgdDesc.indexOf(pathwayPattern);
+        if( pathwayPos>=0 ) {
+            // see if there are patterns for other categories
+            int phenoDisPos = rgdDesc.indexOf(phenoDiseasePattern, pathwayPos);
+
+            int pathwayStopPos = -1;
+            if( phenoDisPos>0 ) {
+                pathwayStopPos = phenoDisPos;
+            } else {
+                int ccPos = rgdDesc.indexOf(ccPattern, pathwayPos);
+                if( ccPos>0 ) {
+                    pathwayStopPos = ccPos;
+                } else {
+                    int chebiPos = rgdDesc.indexOf(chebiPattern, pathwayPos);
+                    if( chebiPos>0 ) {
+                        pathwayStopPos = chebiPos;
+                    } else {
+                        pathwayStopPos = rgdDesc.length();
+                    }
+                }
+            }
+            pathwayDesc = rgdDesc.substring(pathwayPos, pathwayStopPos);
+        }
+
+        int chebiPos = rgdDesc.indexOf(chebiPattern, pathwayPos);
+        if( chebiPos>=0 ) {
+            chebiDesc = rgdDesc.substring(chebiPos);
+        }
+
+        // append pathway and chebi desc to the AGR desc
+        String mergedDesc = agrDesc.substring(0, agrDesc.length()-1);
+        if( pathwayDesc!=null ) {
+            mergedDesc +=  "; " + pathwayDesc;
+        }
+        if( chebiDesc!=null ) {
+            mergedDesc +=  "; " + chebiDesc;
+        }
+        mergedDesc += ".";
+        return mergedDesc;
     }
 
     // return local file name
