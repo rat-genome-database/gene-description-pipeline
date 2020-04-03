499522b00c87104eb0a35dacfa1b1a92eedff1d0
tutajm <mtutaj@mcw.edu>
2020-04-03 12:01:06 -0500

    implemented latest file logic due to discontinuation of of stable urls

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ Generate merged gene descriptions for rat genes based on:
 
 Automated gene descriptions can be downloaded from AGR in two ways:
  1) by downloading gene record via JSON API and parsing JSON file
- 2) by downloading species-specific gene description file from AGR download area
+ 2) by downloading species-specific gene description file in tsv format from AGR download area
+    
+Since April 2020 AGR does not provide stable file paths anymore. Therefore the logic is to first
+query AGR FMS about the latest file, and then that file will be downloaded.
+
+Example for rat, as of Apr 3, 2020:
+ 1) download https://fms.alliancegenome.org/api/datafile/by/GENE-DESCRIPTION-TSV/RGD?latest=true
+ 2) extract "s3Path" property: which happens to have the value of "3.0.0/GENE-DESCRIPTION-TSV/RGD/GENE-DESCRIPTION-TSV_RGD_21.tsv"
+ 3) download actual file "https://download.alliancegenome.org/3.0.0/GENE-DESCRIPTION-TSV/RGD/GENE-DESCRIPTION-TSV_RGD_21.tsv"
+ 4) parse and process the downloaded file     
--- src/main/java/edu/mcw/rgd/pipelines/GeneDesc/AgrGeneDescViaTsvFile.java
+++ src/main/java/edu/mcw/rgd/pipelines/GeneDesc/AgrGeneDescViaTsvFile.java
@@ -1,11 +1,11 @@
 package edu.mcw.rgd.pipelines.GeneDesc;
 
-
 import edu.mcw.rgd.process.FileDownloader;
 import edu.mcw.rgd.process.Utils;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +17,8 @@ public class AgrGeneDescViaTsvFile extends AgrGeneDesc {
     private Map<String, String> descMap = new HashMap<>();
     private int geneCountNotInAgr;
     private Map<String,String> geneDescFiles;
+    private Map<String,String> latestFiles;
+    private String downloadUrlPrefix;
 
     public String getGeneralInfo() {
         return "   automated gene descriptions will be downloaded via TSV FILE";
@@ -25,17 +27,13 @@ public class AgrGeneDescViaTsvFile extends AgrGeneDesc {
     public void init(String speciesName) throws Exception {
 
         // there must be a species file
-        String geneDescFile = getGeneDescFiles().get(speciesName);
-        if( geneDescFile==null ) {
-            throw new Exception("ERROR! species "+speciesName+" does not have a gene description file name defined!");
-        }
-        super.init(speciesName);
+        String latestFileUrl = getLatestFileUrlForSpecies(speciesName);
 
         FileDownloader fd = new FileDownloader();
         fd.setUseCompression(true);
         fd.setPrependDateStamp(true);
-        fd.setExternalFile(geneDescFile);
-        fd.setLocalFile("data/desc_"+speciesName+".txt.gz");
+        fd.setExternalFile(latestFileUrl);
+        fd.setLocalFile("data/desc_"+speciesName+".json.gz");
 
         String localFile = fd.downloadNew();
 
@@ -62,6 +60,57 @@ public class AgrGeneDescViaTsvFile extends AgrGeneDesc {
         geneCountNotInAgr = 0;
     }
 
+    String getLatestFileUrlForSpecies(String speciesName) throws Exception {
+
+        // there must be a species file
+        String latestFileQuery = getLatestFiles().get(speciesName);
+        if( latestFileQuery==null ) {
+            throw new Exception("ERROR! species "+speciesName+" does not have a latest file entry defined!");
+        }
+        super.init(speciesName);
+
+        FileDownloader fd = new FileDownloader();
+        fd.setUseCompression(true);
+        fd.setPrependDateStamp(true);
+        fd.setExternalFile(latestFileQuery);
+        fd.setLocalFile("data/latest_"+speciesName+".json.gz");
+
+        String localFile = fd.downloadNew();
+
+        // downloaded file content is json; example:
+        // "s3Path":"3.0.0/GENE-DESCRIPTION-TSV/RGD/GENE-DESCRIPTION-TSV_RGD_21.tsv"
+
+        // extract the content of "s3Path" attribute
+        String json = readCompressedFileAsString(localFile);
+        int s3PathPos = json.indexOf("\"s3Path\"");
+        if( s3PathPos<0 ) {
+            throw new Exception("Cannot find 's3Path' in contents of file "+localFile);
+        }
+        int dblQuoteStartPos = json.indexOf('\"', s3PathPos+8);
+        if( dblQuoteStartPos <= s3PathPos ) {
+            throw new Exception("Unexpected: not found start double quote for value of s3Path");
+        }
+        int dblQuoteStopPos = json.indexOf('\"', dblQuoteStartPos+1);
+        if( dblQuoteStopPos <= dblQuoteStartPos ) {
+            throw new Exception("Unexpected: not found stop double quote for value of s3Path");
+        }
+        String latestFilePath = json.substring(dblQuoteStartPos+1, dblQuoteStopPos);
+        String fullUrl = getDownloadUrlPrefix()+latestFilePath;
+        return fullUrl;
+    }
+
+    String readCompressedFileAsString(String fileName) throws IOException {
+        StringBuffer buf = new StringBuffer();
+        BufferedReader in = Utils.openReader(fileName);
+        String line;
+        while( (line=in.readLine())!=null ) {
+            buf.append(line);
+            buf.append("\n");
+        }
+        in.close();
+        return buf.toString();
+    }
+
     public String getAutoGeneDesc(String curie) throws Exception {
         String autoDesc = descMap.get(curie);
         if( autoDesc==null ) {
@@ -84,5 +133,21 @@ public class AgrGeneDescViaTsvFile extends AgrGeneDesc {
 
     public Map<String,String> getGeneDescFiles() {
         return geneDescFiles;
+    }
+
+    public void setLatestFiles(Map<String,String> latestFiles) {
+        this.latestFiles = latestFiles;
+    }
+
+    public Map<String,String> getLatestFiles() {
+        return latestFiles;
+    }
+
+    public String getDownloadUrlPrefix() {
+        return downloadUrlPrefix;
+    }
+
+    public void setDownloadUrlPrefix(String downloadUrlPrefix) {
+        this.downloadUrlPrefix = downloadUrlPrefix;
     }
 }
