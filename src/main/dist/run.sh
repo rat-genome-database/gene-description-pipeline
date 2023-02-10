2f6423a9ed63825ca9d621aa91f063285a6ac2f2
tutajm <mtutaj@mcw.edu>
2023-02-10 10:09:31 -0600

    updated dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -15,4 +15,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/${APPNAME}.jar "$@" --tsvFile > run.log 2>&1
 
-mailx -s "[$SERVER] Gene Description Pipeline run" mtutaj@mcw.edu < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] Gene Description Pipeline OK" mtutaj@mcw.edu < $APPDIR/logs/summary.log
